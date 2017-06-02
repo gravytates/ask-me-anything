dfcf943dd7c25896b030577895aca3c1b7fadf90
Grady L Shelton <gradyish@gmail.com>
2017-06-02 10:26:34 -0700

    add delete functionality for questions

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteQuestion(question) {
+      if (confirm("You're about to delete this question")) {
+        this.sendAction('destroyQuestion', question);
+      }
+    }
+  }
 });
